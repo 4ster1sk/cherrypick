90216ee1cc0e3d21172896a51c78d54e40a2e417
4sterisk <4sterisk@ymail.ne.jp>
2026-01-05 20:33:24 +0900

    Revert "fix(backend): 정지되지 않은 원격 사용자가 로컬과 상호 작용할 수 없음 (misskey-dev/misskey#13890)"
    This reverts commit 77848eb3630c7bf2f237ac11269705e5718625ab, reversing
    changes made to fcecc3338dc601d39da626ce0ec672c7ac32e679.

--- packages/backend/src/queue/processors/DeleteAccountProcessorService.ts
+++ packages/backend/src/queue/processors/DeleteAccountProcessorService.ts
@@ -16,7 +16,6 @@ import { bindThis } from '@/decorators.js';
 import { SearchService } from '@/core/SearchService.js';
 import { PageService } from '@/core/PageService.js';
 import { UserEntityService } from '@/core/entities/UserEntityService.js';
-import { GlobalEventService } from '@/core/GlobalEventService.js';
 import { QueueLoggerService } from '../QueueLoggerService.js';
 import type * as Bull from 'bullmq';
 import type { DbUserDeleteJobData } from '../types.js';
@@ -47,7 +46,6 @@ export class DeleteAccountProcessorService {
 		private emailService: EmailService,
 		private queueLoggerService: QueueLoggerService,
 		private searchService: SearchService,
-		private globalEventService: GlobalEventService,
 	) {
 		this.logger = this.queueLoggerService.logger.createSubLogger('delete-account');
 	}
@@ -158,9 +156,6 @@ export class DeleteAccountProcessorService {
 		// nop
 		} else {
 			await this.usersRepository.delete(job.data.user.id);
-
-			// Trigger CacheService
-			this.globalEventService.publishInternalEvent('remoteUserUpdated', { id: job.data.user.id });
 		}
 
 		return `[${job.data.user.id}] Account deleted`;
