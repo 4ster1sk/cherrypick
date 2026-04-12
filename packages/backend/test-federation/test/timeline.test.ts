49f5884cd4c258e8693e69b3080e40176e4f5cb9
4sterisk <4sterisk@ymail.ne.jp>
2026-04-12 05:56:00 +0000

    fix(test)

--- packages/backend/test-federation/test/timeline.test.ts
+++ packages/backend/test-federation/test/timeline.test.ts
@@ -105,9 +105,10 @@ describe('Timeline', () => {
 				await postAndCheckReception(homeTimeline, true, { visibility: 'specified', visibleUserIds: [bobInA.id] });
 			});
 
+			/* yojo-artではlocalOnlyが存在しないためテストから除外
 			test('Don\'t receive remote followee\'s localOnly Note', async () => {
 				await postAndCheckReception(homeTimeline, false, { localOnly: true });
-			});
+			});*/
 
 			test('Don\'t receive remote followee\'s invisible specified-only Note', async () => {
 				await postAndCheckReception(homeTimeline, false, { visibility: 'specified' });
